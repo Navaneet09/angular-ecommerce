b708c6ae8816102192b50ec0b27928d37d343f9c
Navaneet74 <32030378+Navaneet09@users.noreply.github.com>
2025-09-10 20:34:14 +0530

    tried to change the app to provideZoneChangeDetection ... didn't work :(

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,7 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, 
+        provideBrowserGlobalErrorListeners,
+        provideZonelessChangeDetection  
+        } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -13,3 +16,20 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes), provideClientHydration(withEventReplay())
   ]
 };
+
+//the below just changes provideZonelessChangeDetection to provideZoneChangeDetection so changing this will help in not having updates delayed from the application
+// to the web browser
+
+// import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+// import { provideRouter } from '@angular/router';
+
+// import { routes } from './app.routes';
+// import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideBrowserGlobalErrorListeners(),
+//     provideZoneChangeDetection({ eventCoalescing: true }),
+//     provideRouter(routes), provideClientHydration(withEventReplay())
+//   ]
+// };
